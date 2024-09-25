b834dd4df234ce67b01e05db7a14a7c14523f145
MSI\leanh <leanhkhoa2205@gmail.com>
2024-09-25 22:56:27 +0700

    delete folder build

--- build/middleware/error.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var errorMiddleware = function (error, req, res, next) {
-    var status = error.status || 500;
-    var message = error.message || 'Whoops !! something went wrong';
-    res.status(status).json({ status: 'error', message: message });
-};
-exports["default"] = errorMiddleware;
